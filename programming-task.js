48dafb6702d617a1fdf8201fef2ce2baaeaac90d
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-12-08 00:21:31 +1100

    Find the top 3 most active IP addresses only returns single top value

--- programming-task.js
+++ programming-task.js
@@ -1,16 +1,30 @@
 const fs = require('fs');
-console.log('hello');
+console.log('Hello world');
 const logFile = fs.readFileSync("./programming-task-example-data_(1).log", 'utf-8');
 
-// split puts substrings into array
+// split puts substrings of new line into array
 let arrayOfLogFiles = logFile.split("\n");
 
+// splitting data further so I can separate the IP data
 let dataArrayOfLogFiles = [];
 for(let i=0; i<arrayOfLogFiles.length; i++){
+  // if empty string skip / continue
+  if (arrayOfLogFiles[i] == ""){continue}
   let tempArray = arrayOfLogFiles[i].split("-").join(",").split(" ") || [];
   dataArrayOfLogFiles.push(tempArray);  
 };
-console.log(dataArrayOfLogFiles);
 
 let result = dataArrayOfLogFiles.map(ip => ip[0]);
-console.log(result);
+
+// find number of unique ip addresses using ES6 set and spread 
+let uniqueResult = [...new Set(result)]
+console.log(`The number of unique IP addresses ${uniqueResult.length}.`) 
+
+// find the top 3 most active IP addresses
+function mode(result) {
+  return [...result].sort((a,b) =>
+    result.filter(v => v==a).length - result.filter(v => v==b).length
+  ).pop();
+}
+console.log(`The top 3 most active IP addresses ${mode(result)}.`)
+// only returns single top value
