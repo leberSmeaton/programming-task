2fe5d1a55be05654ca3fffadd602da4e274ddbd0
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-12-07 22:52:32 +1100

    split log file into new line arrays with splits - and space joined. returning IP addres as result of index 0 of new arrays. next find number of unique IPs

--- programming-task.js
+++ programming-task.js
@@ -1,3 +1,16 @@
 const fs = require('fs');
 console.log('hello');
-const logFile = require("./programming-task-example-data_(1).log")
+const logFile = fs.readFileSync("./programming-task-example-data_(1).log", 'utf-8');
+
+// split puts substrings into array
+let arrayOfLogFiles = logFile.split("\n");
+
+let dataArrayOfLogFiles = [];
+for(let i=0; i<arrayOfLogFiles.length; i++){
+  let tempArray = arrayOfLogFiles[i].split("-").join(",").split(" ") || [];
+  dataArrayOfLogFiles.push(tempArray);  
+};
+console.log(dataArrayOfLogFiles);
+
+let result = dataArrayOfLogFiles.map(ip => ip[0]);
+console.log(result);
